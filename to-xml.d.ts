7c4a9076b95d5d469b07fa6d29fa5953444162aa
kawanet <u-suke@kawa.net>
2021-04-28 19:01:28 +0900

    TypeScript definition

---
+++ to-xml.d.ts
@@ -0,0 +1,7 @@
+/**
+ * to-xml.d.ts
+ *
+ * @see https://www.npmjs.com/package/to-xml
+ */
+
+export function toXML(value: any, replacer?: (key: string, value: any) => any, space?: string | number): string;
